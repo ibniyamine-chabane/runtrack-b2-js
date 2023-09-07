d4028052e462d9a9d09ae008c161b1e4b00597c5
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-09-07 16:00:57 +0200

    job 01 jour 2 fait et fonctionne

---
+++ jour-02/job-01/script.js
@@ -0,0 +1,19 @@
+
+const background = document.querySelector('html');
+console.log(background)
+// background.style.backgroundColor = "#ffb703";
+
+
+window.addEventListener("resize", myChangeBackgroundColor);
+
+
+function myChangeBackgroundColor() {
+
+        if (window.innerWidth >= 1337) {
+            background.style.backgroundColor = "#ffb703";
+        } else if (window.innerWidth >= 505 && window.innerWidth <= 1336) {
+            background.style.backgroundColor = "#d90429";
+        } else if (window.innerWidth <= 504) {
+            background.style.backgroundColor = "#003049";
+        }
+    };
