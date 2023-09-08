f14cf1b9fa20e99dc325d3ba8ba96c9862472a5e
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-09-08 14:50:55 +0200

    job 02 jour 3 fait

---
+++ jour-03/class/Player.js
@@ -0,0 +1,17 @@
+
+class Player 
+{
+
+    symbole;
+    isCurrentPlayer;
+
+    constructor(symbole, isCurrentPlayer) {
+        this.symbole = symbole;
+        this.isCurrentPlayer = isCurrentPlayer;
+    }
+
+
+}
+
+// let player = new Player("X",false);
+// console.log(player);
